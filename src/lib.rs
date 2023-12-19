27df7e0a03db3003b8b3b865334a93b58c9589f5
angael <krzysztofwidacki@gmail.com>
2023-12-19 12:33:12 +0100

    Refactor code to use Config struct and implement build method
    This commit refactors the code to use a `Config` struct and implements a `build` method to parse command line arguments. This improves code organization and readability.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,27 @@
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-}
+// pub struct Config {
+//     pub query: String,
+//     pub file_path: String,
+// }
 
-pub fn run(config: Config) {
-    println!("Query: {:?}", config.query);
-    println!("File name: {:?}", config.file_path);
+// impl Config {
+//     fn build(args: &[String]) -> Result<Config, &'static str> {
+//         if args.len() < 3 {
+//             return Err("not enough arguments");
+//         }
 
-    let contents =
-        std::fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
+//         let query = args[1].clone();
+//         let file_path = args[2].clone();
 
-    println!("Content: {:?}", contents);
-}
+//         Ok(Config { query, file_path })
+//     }
+// }
+
+// pub fn run(config: Config) {
+//     println!("Query: {:?}", config.query);
+//     println!("File name: {:?}", config.file_path);
+
+//     let contents =
+//         std::fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
+
+//     println!("Content: {:?}", contents);
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
-use std::env;
-
-use minigrep::{run, Config};
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Args: {:?}", args);
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     run(config);
 }
 
-fn parse_config(args: &[String]) -> Config {
-    Config {
-        query: args[1].clone(),
-        file_path: args[2].clone(),
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) {
+    println!("Query: {:?}", config.query);
+    println!("File name: {:?}", config.file_path);
+
+    let contents =
+        std::fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
+
+    println!("Content: {:?}", contents);
 }
